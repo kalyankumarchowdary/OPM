ba5ac1edd9f5dd72079f6c665c164adec7956ead
Malepati Kalyan Kumar Chowdary <malepati.chowdary@nagra.com>
2024-04-19 13:02:09 +0530

    Using @WebServlet annotation for servlet mapping instead of using web.xml

--- opm-app/src/main/java/com/project/opm/Main.java
+++ opm-app/src/main/java/com/project/opm/Main.java
@@ -1,5 +1,6 @@
 package com.project.opm;
 
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -7,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("opm/hello")
 public class Main extends HttpServlet {
     public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
         PrintWriter out = res.getWriter();
